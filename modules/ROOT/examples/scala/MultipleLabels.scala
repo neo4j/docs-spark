3baa969be71e7e70e23baf711ec3a94a26e03cd7
Nicola Vitucci <nicola.vitucci@neo4j.com>
2024-06-04 11:10:36 +0100

    Add Python examples for multiple labels r/w

---
+++ modules/ROOT/examples/scala/MultipleLabels.scala
@@ -0,0 +1,46 @@
+// tag::setup[]
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+// Replace with the actual connection URI and credentials
+val url = "neo4j://localhost:7687"
+val username = "neo4j"
+val password = "password"
+
+val spark = SparkSession.builder
+  .config("neo4j.url", url)
+  .config("neo4j.authentication.basic.username", username)
+  .config("neo4j.authentication.basic.password", password)
+  .getOrCreate()
+// end::setup[]
+
+case class Person(name: String, surname: String, age: Int)
+
+val peopleDF = List(
+  Person("John", "Doe", 42),
+  Person("Jane", "Doe", 40)
+).toDF()
+
+// tag::code-write[]
+peopleDF.write
+  .format("org.neo4j.spark.DataSource")
+  .mode(SaveMode.Append)
+  // ":Person:Employee" and "Person:Employee"
+  // are equivalent
+  .option("labels", ":Person:Employee")
+  .save()
+// end::code-write[]
+
+// tag::code-read[]
+val df = spark.read
+  .format("org.neo4j.spark.DataSource")
+  // ":Person:Employee" and "Person:Employee"
+  // are equivalent
+  .option("labels", ":Person:Employee")
+  .load()
+
+df.show()
+// end::code-read[]
+
+// tag::check[]
+// TODO: add read query to check
+// end::check[]
